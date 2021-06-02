1de935e95f2a6f82cd64c276787925a3cd3a278e
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-06-02 15:23:59 -0400

    min and max heap

---
+++ GenericsDemo/GenericHeap/MaxHeap.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GenericHeap
+{
+    public class MaxHeap<T> where T : IComparable<T>
+    {
+        List<T> _elements = new List<T>();
+
+        public void Add(T toAdd)
+        {
+            _elements.Add(toAdd);
+            SiftDown();
+        }
+
+        public void Remove(T toRemove)
+        {
+            foreach(T el in _elements)
+            {
+                if(el.CompareTo(toRemove) == 0)
+                {
+                    _elements.Remove(toRemove);
+                    SiftDown();
+                    break;
+                }
+            }
+        }
+
+        public T Peek()
+        {
+            if (_elements.Count > 0) return _elements[0];
+            return default(T);
+        }
+
+        public T Pop()
+        {
+            T toReturn = default(T);
+            if(_elements.Count > 0)
+            {
+                toReturn = _elements[0];
+                T lastElement = _elements.TakeLast(1).Single();
+                _elements.Remove(lastElement);
+                _elements[0] = lastElement;
+                if (_elements.Count > 1 && lastElement.CompareTo(toReturn) < 0)
+                    SiftDown();
+            }
+
+            return toReturn;
+        }
+
+        private void SiftDown()
+        {
+            for(int i = 0; i < _elements.Count; i++)
+            {
+                T child, parent = _elements[i];
+                if(2 * i + 1 < _elements.Count && _elements[2 * i + 1].CompareTo(parent) > 0)
+                {
+                    child = _elements[2 * i + 1];
+                    _elements[2 * i + 1] = parent;
+                    _elements[i] = child;
+                    SiftDown();
+                } else if(2 * i + 2 < _elements.Count && _elements[2 * i + 2].CompareTo(parent) > 0)
+                {
+                    child = _elements[2 * i + 2];
+                    _elements[2 * i + 2] = parent;
+                    _elements[i] = child;
+                    SiftDown();
+                }
+            }
+        }
+    }
+}
