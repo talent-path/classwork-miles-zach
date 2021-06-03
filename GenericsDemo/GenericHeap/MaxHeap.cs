994de9921cbe5ef342d03cfc7671aeec9e71bb3e
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-06-03 09:38:46 -0400

    min and max heap update

--- GenericsDemo/GenericHeap/MaxHeap.cs
+++ GenericsDemo/GenericHeap/MaxHeap.cs
@@ -36,14 +36,17 @@ namespace GenericHeap
         public T Pop()
         {
             T toReturn = default(T);
-            if(_elements.Count > 0)
+            if (_elements.Count > 0)
             {
                 toReturn = _elements[0];
                 T lastElement = _elements.TakeLast(1).Single();
-                _elements.Remove(lastElement);
-                _elements[0] = lastElement;
-                if (_elements.Count > 1 && lastElement.CompareTo(toReturn) < 0)
-                    SiftDown();
+                _elements.Remove(toReturn);
+                if (_elements.Count > 1)
+                {
+                    _elements[0] = lastElement;
+                    if (lastElement.CompareTo(toReturn) < 0)
+                        SiftDown();
+                }
             }
 
             return toReturn;
@@ -51,16 +54,18 @@ namespace GenericHeap
 
         private void SiftDown()
         {
-            for(int i = 0; i < _elements.Count; i++)
+            for(int i = 0; i < _elements.Count / 2; i++)
             {
                 T child, parent = _elements[i];
-                if(2 * i + 1 < _elements.Count && _elements[2 * i + 1].CompareTo(parent) > 0)
+                if(2 * i + 1 < _elements.Count
+                    && _elements[2 * i + 1].CompareTo(parent) > 0)
                 {
                     child = _elements[2 * i + 1];
                     _elements[2 * i + 1] = parent;
                     _elements[i] = child;
                     SiftDown();
-                } else if(2 * i + 2 < _elements.Count && _elements[2 * i + 2].CompareTo(parent) > 0)
+                } else if(2 * i + 2 < _elements.Count
+                    && _elements[2 * i + 2].CompareTo(parent) > 0)
                 {
                     child = _elements[2 * i + 2];
                     _elements[2 * i + 2] = parent;
