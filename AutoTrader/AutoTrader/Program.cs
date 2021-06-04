197f701f6a7e902a1f82828d6c17320ef5ae3620
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-06-04 14:38:38 -0400

    auto trader complete

--- AutoTrader/AutoTrader/Program.cs
+++ AutoTrader/AutoTrader/Program.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace AutoTrader
 {
@@ -12,7 +11,7 @@ namespace AutoTrader
     {
         static readonly HttpClient _httpClient = new HttpClient();
         static readonly Mutex _lock = new Mutex();
-        static Dictionary<string, List<string>> stockPrices = new Dictionary<string, List<string>>();
+        static Dictionary<string, List<decimal>> _stockPrices = new Dictionary<string, List<decimal>>();
         static void Main(string[] args)
         {
             //As a user, I should be able to enter a list of stock symbols to watch(just pressing enter to indicate I'm done with a blank line).
@@ -24,7 +23,7 @@ namespace AutoTrader
             List<Task> tasks = new List<Task>();
             foreach (string ticker in tickers)
             {
-                tasks.Add(Task.Factory.StartNew(() => GetStockPrices(ticker)));
+                tasks.Add(Task.Factory.StartNew(() => GetStockPrices(ticker)).Unwrap());
             }
 
             Task.WaitAll(tasks.ToArray());
@@ -46,6 +45,19 @@ namespace AutoTrader
             return tickers;
         }
 
+        static decimal MedianStockPrice(List<decimal> prices)
+        {
+            prices.Sort();
+            if(prices.Count % 2 == 0)
+            {
+                return (prices[(prices.Count / 2) - 1] + prices[prices.Count / 2]) / 2;
+            }
+            else
+            {
+                return prices[prices.Count / 2];
+            }
+        }
+
         static async Task GetStockPrices(string ticker)
         {
             while (true)
@@ -56,20 +68,33 @@ namespace AutoTrader
                     HttpResponseMessage res = await _httpClient.GetAsync($"https://finnhub.io/api/v1/quote?symbol={ticker}&token=c2t4il2ad3i9opckhnr0");
                     res.EnsureSuccessStatusCode();
                     body = await res.Content.ReadAsStringAsync();
+                    Match match = Regex.Match(body, @"(?<={""c"":)[^,]+");
+                    decimal price = decimal.Parse(match.ToString());
+                    Console.WriteLine($"{DateTime.UtcNow} {ticker}: ${price}");
                     _lock.WaitOne();
-                    Console.WriteLine(body);
+                    if (_stockPrices.ContainsKey(ticker))
+                    {
+                        if (_stockPrices.GetValueOrDefault(ticker).Count > 2)
+                        {
+                            decimal median = MedianStockPrice(_stockPrices.GetValueOrDefault(ticker));
+                            if (price > median) Console.WriteLine($"Sell signal for {ticker}");
+                            else if (price < median) Console.WriteLine($"Buy signal for {ticker}");
+                        }
+                        _stockPrices.GetValueOrDefault(ticker).Add(price);
+                    }
+                    else
+                    {
+                        _stockPrices.Add(ticker, new List<decimal> { price });
+                    }
                     _lock.ReleaseMutex();
                 } catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                 }
-                finally
-                {
-                    Console.WriteLine(body);
-                }
-                //await Task.Delay(1000);
+               
+                await Task.Delay(60000);
             }
-            Thread.Sleep(30_000);
+            
         }
     }
 }
