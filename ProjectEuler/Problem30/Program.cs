9bb39870e5fefb6ce709447dde524bce76b2876a
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-06-21 12:51:20 -0400

    euler p30

---
+++ ProjectEuler/Problem30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for(int i = 10; i < 999999; i++)
+            {
+                int s = 0;
+                int c = i;
+                while(c != 0)
+                {
+                    int d = c % 10;
+                    c /= 10;
+                    s += d * d * d * d * d;
+                }
+                if (s == i) sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
